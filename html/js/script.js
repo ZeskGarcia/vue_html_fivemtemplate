33d49511040da7aba1bb1feb37e8302eeecf7b86
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2024-07-28 10:40:39 +0200

    Update script.js

--- html/js/script.js
+++ html/js/script.js
@@ -2,7 +2,9 @@ const app_config = {
     el: '#app',
     data: {},
     methods: {},
-    mounted() {}
+    mounted() {
+        window.addEventListener('message', async ({data}) => this[data.type] && this[data.type](data));
+    }
 };
 
 const app = new Vue(app_config);
