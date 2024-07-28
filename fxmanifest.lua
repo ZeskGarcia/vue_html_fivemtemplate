ce28ba45db81c8959a941f6619199a52f2aa6699
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2024-07-28 10:38:19 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game { 'gta5' }
+
+description 'Vue HTML FiveM Template (UI)'
+
+client_scripts {
+  'client/*.lu*'
+}
+
+ui_page 'html/ui.html'
+files {
+  'html/*.html',
+  'html/js/*.js',
+  'html/css/*.css'
+}
